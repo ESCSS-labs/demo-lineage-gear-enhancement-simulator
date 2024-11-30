9cd1eacd43578fee98ffef9513c6d6c65f97f91b
mike <cspkno005@gmail.com>
2024-11-30 15:56:57 +0800

    refactor test

--- e2e/features.test.js
+++ e2e/features.test.js
@@ -2,7 +2,13 @@ import { test, expect } from "@playwright/test";
 
 test.describe("mouse scroll testing", () => {
   test("F5 stop function test", async ({ page }) => {
-    await page.goto("http://[::]:3000/");
+    page.on('console', msg => {
+      if(msg.type() === 'error') {
+        throw new Error(msg.text())
+      }
+    });
+
+    await page.goto("http://localhost:3000/");
     await page
       .locator("figure")
       .filter({ hasText: "對盔甲施法的卷軸能增加防具的防禦力" })
@@ -15,6 +21,12 @@ test.describe("mouse scroll testing", () => {
   });
 
   test("armor to +1", async ({ page }) => {
+    page.on('console', msg => {
+      if(msg.type() === 'error') {
+        throw new Error(msg.text())
+      }
+    });
+
     await page.goto("http://localhost:3000/");
     await page
       .locator("figure")
@@ -28,6 +40,12 @@ test.describe("mouse scroll testing", () => {
   });
 
   test("armor to -1", async ({ page }) => {
+    page.on('console', msg => {
+      if(msg.type() === 'error') {
+        throw new Error(msg.text())
+      }
+    });
+
     await page.goto("http://localhost:3000/");
     await page
       .locator("figure")
@@ -41,6 +59,12 @@ test.describe("mouse scroll testing", () => {
   });
 
   test("weapon to +1", async ({ page }) => {
+    page.on('console', msg => {
+      if(msg.type() === 'error') {
+        throw new Error(msg.text())
+      }
+    });
+
     await page.goto("http://localhost:3000/");
     await page
       .locator("figure")
@@ -54,82 +78,18 @@ test.describe("mouse scroll testing", () => {
   });
 
   test("weapon to -1", async ({ page }) => {
+    page.on('console', msg => {
+      if(msg.type() === 'error') {
+        throw new Error(msg.text())
+      }
+    });
+
     await page.goto("http://localhost:3000/");
     await page
       .locator("figure")
       .filter({ hasText: "對武器施法的卷軸強化成功可將武器強化數值(-1)" })
       .locator('[id="🔥ItemsUI__X__Img"]')
       .click();
-    await page.locator('[id="🔥StatusEquips__Equip"]').first().click();
-    await expect(page.locator('[id="🌀SinglePlayer__Bottom"]')).toContainText(
-      "+0 瑟魯基之劍 一瞬間發出 黑色的 光芒。",
-    );
-  });
-});
-
-test.describe("keyboard scroll testing", () => {
-  test("keyboard F5 stop function test", async ({ page }) => {
-    await page.goto("http://[::]:3000/");
-    await page.locator("body").press("F6");
-    await page.locator("body").press("F5");
-    await page.locator("li:nth-child(10)").click();
-    await expect(page.locator('[id="🌀SinglePlayer__Bottom"]')).toContainText(
-      "請選擇一種防具。",
-    );
-  });
-
-  test("keyboard F9 stop function test", async ({ page }) => {
-    await page.goto("http://[::]:3000/");
-    await page.locator("body").press("F6");
-    await page.locator("body").press("F9");
-    await page.locator("li:nth-child(10)").click();
-    await expect(page.locator('[id="🌀SinglePlayer__Bottom"]')).toContainText(
-      "請選擇一種防具。",
-    );
-  });
-
-  test.slow("armor to +1", async ({ page }) => {
-    await page.goto("http://[::]:3000/");
-    await page.locator("body").press("F6");
-    await page.locator("li:nth-child(10)").click();
-    await expect(page.locator('[id="🌀SinglePlayer__Bottom"]')).toContainText(
-      "+0 力量手套 一瞬間發出 銀色的 光芒。",
-    );
-    await expect(page.locator('[id="🔥NumbersUI__Line2__Ac"]')).toContainText(
-      "-8",
-    );
-    await expect(
-      page.locator('[id="🔥StatusNumbers__Basic__Li"]:nth-of-type(3)'),
-    ).toContainText("-8");
-  });
-
-  test("armor to -1", async ({ page }) => {
-    await page.goto("http://[::]:3000/");
-    await page.locator("body").press("F8");
-    await page.locator("li:nth-child(10)").click();
-    await expect(page.locator('[id="🌀SinglePlayer__Bottom"]')).toContainText(
-      "+0 力量手套 一瞬間發出 黑色的 光芒。",
-    );
-    await expect(page.locator('[id="🔥NumbersUI__Line2__Ac"]')).toContainText(
-      "-6",
-    );
-    await expect(
-      page.locator('[id="🔥StatusNumbers__Basic__Li"]:nth-of-type(3)'),
-    ).toContainText("-6");
-  });
-
-  test("weapon to +1", async ({ page }) => {
-    await page.goto("http://[::]:3000/");
-    await page.locator("body").press("F10");
-    await page.locator('[id="🔥StatusEquips__Equip"]').first().click();
-    await expect(page.locator('[id="🌀SinglePlayer__Bottom"]')).toContainText(
-      "+0 瑟魯基之劍 一瞬間發出 藍色的 光芒。",
-    );
-  });
-
-  test("weapon to -1", async ({ page }) => {
-    await page.goto("http://[::]:3000/");
-    await page.locator("body").press("F12");
     await page.locator('[id="🔥StatusEquips__Equip"]').first().click();
     await expect(page.locator('[id="🌀SinglePlayer__Bottom"]')).toContainText(
       "+0 瑟魯基之劍 一瞬間發出 黑色的 光芒。",
